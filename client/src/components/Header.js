356d03b6e74db9342c84b0549a22883fa95dab23
REDOWAN038 <redowanibrahim2724@gmail.com>
2023-09-26 11:39:26 +0600

    header upper part

---
+++ client/src/components/Header.js
@@ -0,0 +1,94 @@
+import React from "react"
+import { Link, NavLink } from "react-router-dom"
+import { BsSearch } from "react-icons/bs"
+
+const Header = () => {
+  return (
+    <>
+      <header className='header-top-strip py-3'>
+        <div className='container-xxl'>
+          <div className='row'>
+            <div className='col-6'>
+              <p className='text-white mb-0'>
+                Free Shipping Over $100 and Free returns
+              </p>
+            </div>
+            <div className='col-6'>
+              <p className='text-end text-white mb-0'>
+                Hotline :{" "}
+                <a className='text-white' href='tel:+880 1234567891'>
+                  +880 1234567891
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </header>
+
+      <header className='header-upper py-3'>
+        <div className='container-xxl'>
+          <div className='row align-items-center'>
+            <div className='col-2'>
+              <h2>
+                <Link className='text-white'>RED2724</Link>
+              </h2>
+            </div>
+            <div className='col-5'>
+              <div className='input-group'>
+                <input
+                  type='text'
+                  className='form-control py-2'
+                  placeholder='Search Product Here ...'
+                  aria-label='Search Product Here ...'
+                  aria-describedby='basic-addon2'
+                />
+                <span className='input-group-text p-3' id='basic-addon2'>
+                  <BsSearch className='fs-6' />
+                </span>
+              </div>
+            </div>
+            <div className='col-5'>
+              <div className='header-upper-links'>
+                <div>
+                  <Link className='d-flex align-items-center gap-10 text-white'>
+                    <img src='images/compare.svg' alt='compare' />
+                    <p className='mb-0'>
+                      Compare <br /> Products
+                    </p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className='d-flex align-items-center gap-10 text-white'>
+                    <img src='images/wishlist.svg' alt='wishlist' />
+                    <p className='mb-0'>
+                      Favourite <br /> Wishlist
+                    </p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className='d-flex align-items-center gap-10 text-white'>
+                    <img src='images/user.svg' alt='user' />
+                    <p className='mb-0'>
+                      Log In <br /> My Account
+                    </p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className='d-flex align-items-center gap-10 text-white'>
+                    <img src='images/cart.svg' alt='cart' />
+                    <div className='d-flex flex-column gap-10'>
+                      <span className='badge bg-white text-dark'>0</span>
+                      <p className='mb-0'>$ 500</p>
+                    </div>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  )
+}
+
+export default Header
